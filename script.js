1e429f97f32a4abec3227a0530d2815cb1d8a17e
sikandrahere <148213515+sikandrahere@users.noreply.github.com>
2024-06-07 12:10:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const toggleButton =document.getElementById('toggle');
+        const navLinks=document.getElementById('menu-bar');
+        toggleButton.addEventListener('click',()=>{
+            navLinks.classList.toggle('active');
+        })
+
+
+        
+     
+        
